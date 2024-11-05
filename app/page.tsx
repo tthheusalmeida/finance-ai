5a584ae26684d42d6964d811feac98371d0f7097
Matheus Almeida <tthheusalmeida@gmail.com>
2024-11-05 10:48:52 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
-  return <h1 className="p-5 text-red-500">Home Page</h1>;
+  return <Button>Hello World!</Button>;
 };
 
 export default Home;

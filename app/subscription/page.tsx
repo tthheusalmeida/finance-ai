3bab95157cfea285615d1cb86dfd215f1a381006
Matheus Almeida <tthheusalmeida@gmail.com>
2024-11-11 11:06:56 -0300

    feat: add stripe webhook that handles subscriptions

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,16 +1,18 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import Navbar from "../_components/navbar";
 import { redirect } from "next/navigation";
 import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import { CheckIcon, XIcon } from "lucide-react";
 import AcquirePlanButton from "./_components/acquire-plan-button";
+import { Badge } from "../_components/ui/badge";
 
 const SubscriptionPage = async () => {
   const { userId } = await auth();
   if (!userId) {
     redirect("/login");
   }
-
+  const user = await clerkClient().users.getUser(userId);
+  const hasPremiumPlan = user.publicMetadata.subscriptionPlan == "premium";
   return (
     <>
       <Navbar />
@@ -26,15 +28,15 @@ const SubscriptionPage = async () => {
               <div className="flex items-center justify-center gap-3">
                 <span className="text-4xl">R$</span>
                 <span className="text-6xl font-semibold">0</span>
-                <span className="text-2xl text-muted-foreground">/mês</span>
+                <div className="text-2xl text-muted-foreground">/mês</div>
               </div>
             </CardHeader>
             <CardContent className="space-y-6 py-8">
-              <div className="item-center flex gap-2">
+              <div className="flex items-center gap-2">
                 <CheckIcon className="text-primary" />
                 <p>Apenas 10 transações por mês (7/10)</p>
               </div>
-              <div className="item-center flex gap-2">
+              <div className="flex items-center gap-2">
                 <XIcon />
                 <p>Relatórios de IA</p>
               </div>
@@ -42,22 +44,27 @@ const SubscriptionPage = async () => {
           </Card>
 
           <Card className="w-[450px]">
-            <CardHeader className="border-b border-solid py-8">
+            <CardHeader className="relative border-b border-solid py-8">
+              {hasPremiumPlan && (
+                <Badge className="absolute left-4 top-12 bg-primary/10 text-primary">
+                  Ativo
+                </Badge>
+              )}
               <h2 className="text-center text-2xl font-semibold">
                 Plano Premium
               </h2>
               <div className="flex items-center justify-center gap-3">
                 <span className="text-4xl">R$</span>
                 <span className="text-6xl font-semibold">19</span>
-                <span className="text-2xl text-muted-foreground">/mês</span>
+                <div className="text-2xl text-muted-foreground">/mês</div>
               </div>
             </CardHeader>
             <CardContent className="space-y-6 py-8">
-              <div className="item-center flex gap-2">
+              <div className="flex items-center gap-2">
                 <CheckIcon className="text-primary" />
                 <p>Transações ilimitadas</p>
               </div>
-              <div className="item-center flex gap-2">
+              <div className="flex items-center gap-2">
                 <CheckIcon className="text-primary" />
                 <p>Relatórios de IA</p>
               </div>
